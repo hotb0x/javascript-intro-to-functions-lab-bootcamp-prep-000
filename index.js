ad43ade94b0d64adfe30f4fdeff7f383b6e68b02
Jesus Garcia <jgarcia.network@gmail.com>
2018-05-24 22:28:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function sayHiToGrandma(string){
     return "I can\'t hear you!"
   } else if (x === "I love you, Grandma") {
     return "I love you, too."
-  } else (x.toUpperCase === upperCase){
+  } else if (x.toUpperCase === upperCase){
     return "YES INDEED!"
   }
 }
