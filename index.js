084817da62eddbb97b24e23ab9d61e30ef69e2f2
Jesus Garcia <jgarcia.network@gmail.com>
2018-05-24 22:06:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   let x = string
-  if (string.toLowerCase !== string){
+  if (string.toLowerCase === string){
     return "I can\'t hear you!"
   } else if (string === "I love you, Grandma") {
     return "I love you, too."
